99a9f8f6b77d82192fde2bc606e36098ea455131
Assis Ferreira <ferreira.assisruny@gmail.com>
2022-02-26 10:48:21 -0100

    home bottom navigation

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -3,27 +3,57 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:instagram_clone_flutter/shared/themes/app_images.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading:false,
-        title: Image.asset(AppImages.textLogo),
-        elevation: 0,
-        backgroundColor: Colors.white,
-        actions: const [
-          Icon(FontAwesomeIcons.plusSquare, color: Colors.black,),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4),
-            child: Icon(FontAwesomeIcons.paperPlane, color: Colors.black,),
-          ),
-        ],
-      ),
-      body: Container(
-        child: Text("Home"),
-      ),
+      appBar: appBar(),
+      body: body(),
+      bottomNavigationBar: bottomNav(),
     );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: Image.asset(AppImages.textLogo),
+      elevation: 0,
+      backgroundColor: Colors.white,
+      actions: const [
+        Icon(
+          FontAwesomeIcons.plusSquare,
+          color: Colors.black,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 4),
+          child: Icon(
+            FontAwesomeIcons.facebookMessenger,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget body() {
+    return Container(
+      child: Text("Home"),
+    );
+  }
+
+  BottomAppBar bottomNav() {
+    return BottomAppBar(
+        elevation: 0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.home_outlined, size: 32,), padding: EdgeInsets.all(0),),
+            IconButton(onPressed: () {}, icon: Icon(Icons.search, size: 32,)),
+            IconButton(onPressed: () {}, icon: Icon(FontAwesomeIcons.playCircle)),
+            IconButton(onPressed: () {}, icon: Icon(FontAwesomeIcons.heart)),
+            IconButton(onPressed: () {}, icon: Icon(FontAwesomeIcons.user))
+          ],
+        ));
   }
 }
